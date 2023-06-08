815232b42579ac47dff85a409433ca0f5a2c9dcb
cheongha12 <cheong.9396@gmail.com>
2023-06-08 17:44:47 +0900

    BubbleSort

---
+++ 14_List_Music/src/com/kh/practice/sort/BubbleSort.java
@@ -0,0 +1,70 @@
+package com.kh.practice.sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+public static void main(String[] args) {
+	
+	int a = 10;
+	int b = 20;
+	//a 방에 b 값으로 바꾸고, b방에 a값으로 바꾸기
+	//SWAP
+	
+	int temp = a;
+	a = b;
+	b= temp;
+
+	
+	System.out.println(a); //20
+	System.out.println(b);//10
+	
+	
+	
+	
+	//버블정렬
+	int[] arr = new int [] {3,2,5,1,6,8,};
+	System.out.println(Arrays.toString(arr));
+	// 배열 ==> List
+	System.out.println("[");
+	for(int ar : arr) {
+		System.out.println(ar);
+		System.out.println(",");
+	}
+	System.out.println("]");
+	System.out.println(Arrays.asList(arr));
+	for (int i= 0; i < arr.length-1; i++) {
+		for(int j = 0; j < arr.length - 1 - i ;j++) {
+			if(arr[j] > arr[j+1]) {	//오름차순
+	//			if(arr[j] < arr[j+1]) {	//내림차순
+				//SWAP
+				int tmp = arr[j];
+				arr[j]= arr[j+1];
+				arr[j+1] = tmp;
+			}
+			}
+		}
+		System.out.println(arr);
+		
+		String[]nameArr= new String[] {"손범규","천영준","유청하","김민성"};
+		System.out.println("[");
+		for(String ar: nameArr) {
+			System.out.println(ar);
+			System.out.println(",");
+			
+		}
+		System.out.println("]");
+		System.out.println(Arrays.asList(arr));
+		for(int i= 0; i < nameArr.length-1; i++) {
+			for(int j = 0; j <nameArr.length - 1 - i ;j++) {
+				if(nameArr[j].compareTo(nameArr[j+1]) > 0) {
+					String tmp = nameArr[j];
+					nameArr[j] = nameArr[j+1];
+					nameArr[j+1] = tmp;
+				}
+			}
+			
+		}
+		
+	}
+
+}
